7d9a96517554e68f9ce4baf633f5337d84413a7c
Fuad Hasan <fuad.cs22@gmail.com>
2024-10-14 22:41:27 +0600

    Add page title with company name for better SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,7 @@
   
   <svelte:head>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <title>{companyName} - Next-Gen Software Solutions</title>
   </svelte:head>
   
   <main>
